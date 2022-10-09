a5cc04ae1e3343fd5f8175f24b13fed1b6976ca8
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-09 01:08:11 -0400

    mvp commit for var for numbers.py

---
+++ index.py
@@ -0,0 +1,39 @@
+# There are two correct answers for this one. Answer with either one. What is this?
+100
+# number
+
+# Type the illegal character in this variable name.
+# 2nd_runner_up
+# 2
+
+# In the orange box type the next character. I'll autocomplete. Don't type spaces or carriage returns.
+# Assign the number 5 to the variable players
+players = 5
+
+# Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces. When the exercise is complete, I'll turn the exercise number green.
+# Assign the negative number -6 to the variable loss
+loss = -6
+
+# What is the value of total?
+number = 10
+total = number + 2
+
+print(total)
+# 12
+
+# Assign a number (your choice) to the variable total.
+total = 1
+
+# Assign a negative integer to a variable. You choose the integer and the variable name.
+neg_num = -10
+
+# What is this (one word, lowercase)?
+# "-8.087"
+
+# Add 5 to the variable running_total and assign the result to the same variable
+running_total = 10
+running_total = running_total + 5
+
+# assign the sum of two numbers to a variable. Make up the variable name and the numbers. On line 2, tell Python to display the sum using the variable.
+total = 10 + 1
+print(total)
